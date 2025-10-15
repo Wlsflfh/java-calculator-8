72202052b9fcfbdbe07bf4a10f10a940e970af82
Jinriro <riro12.hanyang.ac.kr>
2025-10-15 10:37:09 +0900

    feat: 컨트롤러 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,2 +1,21 @@
-package calculator.controller;public class calculatorController {
+package calculator.controller;
+
+import calculator.domain.Expression;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+public class CalculatorController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public CalculatorController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        Expression expression = new Expression(inputView.readExpression());
+        outputView.printResult(expression.calculateSum());
+    }
 }
