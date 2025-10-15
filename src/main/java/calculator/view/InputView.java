067d282ef34930b9f83496445292c41ff277b2ce
Jinriro <riro12.hanyang.ac.kr>
2025-10-15 10:39:37 +0900

    feat: InputView 클래스 생성 및 사용자 입력 처리 기능 구현

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,2 +1,13 @@
-package calculator.view;public class InputView {
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+    public String readExpression() {
+        System.out.println(INPUT_MESSAGE);
+        return Console.readLine();
+    }
 }
