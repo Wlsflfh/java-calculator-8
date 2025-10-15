ab536722a1174fdd15ca87589c82391b9ec9ce8d
Jinriro <riro12.hanyang.ac.kr>
2025-10-15 10:40:30 +0900

    feat: OutputView 클래스 생성 및 출력 기능 구현

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,2 +1,8 @@
-package calculator.view;public class OutputView {
+package calculator.view;
+
+public class OutputView {
+
+    public void printResult(long result) {
+        System.out.printf("결과 : %d\n", result);
+    }
 }
