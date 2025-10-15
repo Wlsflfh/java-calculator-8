fe41590366c5983d579020f5558a356e222bdf8c
Jinriro <riro12.hanyang.ac.kr>
2025-10-15 10:36:08 +0900

    feat: 숫자 입력 검증 로직을 Digit 객체로 캡슐화

--- src/main/java/calculator/domain/Digit.java
+++ src/main/java/calculator/domain/Digit.java
@@ -1,2 +1,33 @@
-package calculator.domain;public class Number {
+package calculator.domain;
+
+public class Digit {
+
+    private final int digit;
+
+    public Digit(String digit) {
+        validateDigit(digit);
+        this.digit = parse(digit);
+    }
+
+    private int parse(String digit) {
+        return Integer.parseInt(digit);
+    }
+
+    private void validateDigit(String digit) {
+        try {
+            validateMaxInteger(Long.parseLong(digit));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 입력 형식입니다.");
+        }
+    }
+
+    private static void validateMaxInteger(long value) {
+        if (value > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("입력값이 정수형 최대 범위를 초과했습니다.");
+        }
+    }
+
+    public int getDigit() {
+        return digit;
+    }
 }
