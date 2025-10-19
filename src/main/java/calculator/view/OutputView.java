189b93143494bc2e03d34c9e74104ca907a5c418
Jinriro <riro12.hanyang.ac.kr>
2025-10-19 16:09:24 +0900

    refactor: 결과 메시지를 상수로 분리

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,7 +2,9 @@ package calculator.view;
 
 public class OutputView {
 
+    private static final String RESULT_MESSAGE = "결과 : %d\n";
+
     public void printResult(long result) {
-        System.out.printf("결과 : %d\n", result);
+        System.out.printf(RESULT_MESSAGE, result);
     }
 }
